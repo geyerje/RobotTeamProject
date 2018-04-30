4cb60164f935d18e34f508edae0a147c51b1d013
geyerje <geyerje@rose-hulman.edu>
2018-04-30 10:40:34 -0400

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	sandbox_motors_new/person3_motors.py

--- sandbox_motors_new/person3_motors.py
+++ sandbox_motors_new/person3_motors.py
@@ -8,7 +8,7 @@ Authors: David Fisher, David Mutchler and James (Bo) Geyer.
 #   Then repeat for turn_left_by_time.
 #   Then repeat for turn_left_by_encoders.
 #   Then repeat for the turn_right functions.
-
+import math
 import ev3dev.ev3 as ev3
 import time
 
@@ -16,17 +16,31 @@ left_motor = ev3.LargeMotor(ev3.OUTPUT_B)
 right_motor = ev3.LargeMotor(ev3.OUTPUT_C)
 
 def test_turn_left_turn_right():
+    time_s = 1
     while time_s != 0:
-        speed = int(input("Enter a speed for the left turn (0 to 100): "))
+        speed = int(input("Enter a speed for the left turn (0 to 100): ")) * 8
         stop = str(input('Enter stop actions: brake, coast, or hold?'))
         time_s = int(input("Enter a time to drive (seconds): "))
         turn_left_seconds(time_s, speed, stop)
-
+    time_s = 1
     while time_s != 0:
-        speed = int(input("Enter a speed for the left turn (0 to 100): "))
+        speed = int(input("Enter a speed for the left turn (0 to 100): ")) * 8
         stop = str(input('Enter stop actions: brake, coast, or hold?'))
         degrees = int(input("Enter degrees to turn through "))
         turn_left_by_time(time_s, speed, stop)
+    time_s = 1
+    while time_s != 0:
+        speed = int(input("Enter a speed for the left turn (0 to 100): ")) * 8
+        stop = str(input('Enter stop actions: brake, coast, or hold?'))
+        degrees = int(input("Enter degrees to turn through "))
+        turn_left_by_encoders(degrees, speed, stop)
+    time_s = 1
+    while time_s != 0:
+        speed = int(input("Enter a speed for the left turn (0 to 100): ")) * 8
+        stop = str(input('Enter stop actions: brake, coast, or hold?'))
+        degrees = int(input("Enter degrees to turn through "))
+        turn_left_by_time(time_s, speed, stop)
+
 
 
 
@@ -48,9 +62,7 @@ def test_turn_left_turn_right():
 
 def turn_left_seconds(time_s, speed, stop):
     right_motor.run_forever(speed_sp=speed)
-    left_motor.run_forever(speed_sp=-speed)
     time.sleep(time_s)
-    left_motor.stop()
     right_motor.stop(stop_action=stop)
     """
     Makes the robot turn in place left for the given number of seconds at the given speed,
@@ -60,10 +72,9 @@ def turn_left_seconds(time_s, speed, stop):
 
 
 def turn_left_by_time(degrees, speed, stop):
-    right_motor.run_to_rel_pos(position_sp=459.457, speed_sp=speed)
-    left_motor.run_to_rel_pos(position_sp=-459.457, speed_sp=speed)
-    right_motor.wait_while(ev3.Motor.STATE_RUNNING)
-    left_motor.wait_while(ev3.Motor.STATE_RUNNING)
+    time = degrees/speed
+    right_motor.run_forever(speed_sp=speed)
+    time.sleep(time)
     right_motor.stop(stop_action=stop)
 
     """
@@ -78,6 +89,13 @@ def turn_left_by_time(degrees, speed, stop):
 
 
 def turn_left_by_encoders(degrees, speed, stop_action):
+    dis = degrees * (2*math.pi/180) * 3.25
+    right_motor.run_to_rel_pos(position_sp=dis, speed_sp=speed)
+    left_motor.run_to_rel_pos(position_sp=-dis, speed_sp=speed)
+    right_motor.wait_while(ev3.Motor.STATE_RUNNING)
+    left_motor.wait_while(ev3.Motor.STATE_RUNNING)
+    right_motor.stop(stop_action)
+
     """
     Makes the robot turn in place left the given number of degrees at the given speed,
     where speed is between -100 (full speed turn_right) and 100 (full speed turn_left).
@@ -87,15 +105,15 @@ def turn_left_by_encoders(degrees, speed, stop_action):
     """
 
 
-def turn_right_seconds(seconds, speed, stop_action=):
+def turn_right_seconds(seconds, speed, stop_action):
     """ Calls turn_left_seconds with negative speeds to achieve turn_right motion. """
 
 
-def turn_right_by_time(degrees, speed, stop_action=):
+def turn_right_by_time(degrees, speed, stop_action):
     """ Calls turn_left_by_time with negative speeds to achieve turn_right motion. """
 
 
-def turn_right_by_encoders(degrees, speed, stop_action=):
+def turn_right_by_encoders(degrees, speed, stop_action):
     """ Calls turn_left_by_encoders with negative speeds to achieve turn_right motion. """
 
 
