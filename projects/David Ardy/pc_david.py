55ec5f4ffb1d511803f2b376f32ad860b872bbb9
ardyd <ardyd@rose-hulman.edu>
2018-05-17 10:56:30 -0400

    $

--- projects/David Ardy/pc_david.py
+++ projects/David Ardy/pc_david.py
@@ -13,7 +13,7 @@ def main():
     root = tkinter.Tk()
     root.title("MQTT Remote")
 
-    main_frame = ttk.Frame(root, padding=20, relief='raised')
+    main_frame = ttk.Frame(root, padding=25, relief='raised')
     main_frame.grid()
 
     left_speed_label = ttk.Label(main_frame, text="Left")
@@ -23,10 +23,10 @@ def main():
     left_speed_entry.grid(row=1, column=0)
 
     right_speed_label = ttk.Label(main_frame, text="Right")
-    right_speed_label.grid(row=0, column=2)
+    right_speed_label.grid(row=0, column=3)
     right_speed_entry = ttk.Entry(main_frame, width=8, justify=tkinter.RIGHT)
     right_speed_entry.insert(0, "600")
-    right_speed_entry.grid(row=1, column=2)
+    right_speed_entry.grid(row=1, column=3)
 
     button_left = ttk.Button(main_frame, text="Left")
     button_left.grid(row=3, column=0)
@@ -35,14 +35,14 @@ def main():
 
     button_right = ttk.Button(main_frame, text="Right")
     button_right.grid(row=3, column=3)
-    button_right['command'] = lambda event: send_right(mqtt_client, left_speed_entry.get(), right_speed_entry.get())
+    button_right['command'] = lambda: send_right(mqtt_client, left_speed_entry.get(), right_speed_entry.get())
     root.bind('<Right>', lambda event: send_right(mqtt_client, left_speed_entry.get(), right_speed_entry.get()))
 
     root.mainloop()
 
 
 def send_left(mqtt_client, left, right):
-    print("robot_left")
+    print("robot_left with left speed ", [-int(left), " and right speed ", right])
     mqtt_client.send_message("move", [-int(left), right])
 
 
