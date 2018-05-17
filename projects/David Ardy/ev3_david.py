5096736cc60cf49a29971c50d111c77449b27325
ardyd <ardyd@rose-hulman.edu>
2018-05-17 11:20:04 -0400

    $

--- projects/David Ardy/ev3_david.py
+++ projects/David Ardy/ev3_david.py
@@ -4,3 +4,16 @@
 #
 #     def blah(self):
 #         self.robot
+
+import mqtt_remote_method_calls as com
+import robot_controller as robo
+
+
+def main():
+    robot = robo.Snatch3r()
+    mqtt_client = com.MqttClient(robot)
+    mqtt_client.connect_to_pc()
+    robot.loop_forever()
+
+
+main()
