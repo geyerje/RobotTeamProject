4cf198da57adf4f06f14e53f54ee028b9da169f2
ardyd <ardyd@rose-hulman.edu>
2018-05-24 00:22:46 -0400

    $

--- projects/David Ardy/Project.py
+++ projects/David Ardy/Project.py
@@ -1,5 +1,7 @@
 def watch_move(self, left_speed, right_speed):
-    while self.color_sensor
+    while self.color_sensor.color != 1:
         self.right_motor.run_forever(speed_sp=right_speed)
         self.left_motor.run_forever(speed_sp=left_speed)
-
+    self.stop_robot()
+    self.right_motor.run_timed(speed_sp=-800, time_sp=100)
+    self.left_motor.run_timed(speed_sp=-800, time_sp=100)
