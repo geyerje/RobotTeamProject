14dc641ea604059b4d1cff379226887bb38e62c5
ardyd <ardyd@rose-hulman.edu>
2018-05-23 21:52:58 -0400

    $

--- projects/Dardy/pc_david.py
+++ projects/Dardy/pc_david.py
@@ -41,15 +41,55 @@ def main():
 
     root.mainloop()
 
+###########################################################
+# Callbacks for robot functions with drive and arm motors #
+###########################################################
 
+
+# turns the robot left by turning left base motor backwards and right base motor forwards
 def send_left(mqtt_client, left, right):
     print("robot_left with left speed", -int(left), "and right speed", right)
     mqtt_client.send_message("move", [-int(left), right])
 
 
+# turns the robot right by turning right base motor backwards and left base motor forwards
 def send_right(mqtt_client, left, right):
     print("robot_right with left speed", left, "and right speed", -int(right))
     mqtt_client.send_message("move", [left, -int(right)])
+
+
+def send_forward(mqtt_client, left, right):
+    print("robot_forward with left speed", left, "and right speed", right)
+    mqtt_client.send_message("move", [left, right])
+
+
+def send_back(mqtt_client, left, right):
+    print("robot_back with left speed", -int(left), "and right speed", -int(right))
+    mqtt_client.send_message("move", [-int(left), -int(right)])
+
+
+def stop(mqtt_client):
+    print("robot_stop")
+    mqtt_client.send_message("stop_robot")
+
+
+def send_up(mqtt_client):
+    print("arm_up")
+    mqtt_client.send_message("arm_up")
+
+
+def send_down(mqtt_client):
+    print("arm_down")
+    mqtt_client.send_message("arm_down")
+
+
+# Quit callbacks to quit program without errors
+def quit_program(mqtt_client, shutdown_ev3):
+    if shutdown_ev3:
+        print("shutdown")
+        mqtt_client.send_message("shutdown")
+    mqtt_client.close()
+    exit()
 
 
 main()
