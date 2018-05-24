efc92ca2279b84a04473c39e14e0583abdd8b8ee
ardyd <ardyd@rose-hulman.edu>
2018-05-24 00:15:50 -0400

    $

---
+++ projects/David Ardy/Project.py
@@ -0,0 +1,5 @@
+def watch_move(self, left_speed, right_speed):
+    while self.color_sensor
+        self.right_motor.run_forever(speed_sp=right_speed)
+        self.left_motor.run_forever(speed_sp=left_speed)
+
